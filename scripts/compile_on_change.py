3cec003e0e27f22d55d7fa526c36a5ff48773cba
Nikita Rudenko <rudenkornk@gmail.com>
2022-03-09 17:22:31 +0300

    Update scripts

--- scripts/compile_on_change.py
+++ scripts/compile_on_change.py
@@ -1,42 +1,11 @@
 #!/usr/bin/env python3
 
+from utils import check_path, check_exists, check_file_if_exists
 import argparse
 import ast
 from pathlib import Path
 import hashlib
-import sys
-import os
-
-
-def error(message):
-    sys.stdout.flush()
-    sys.stderr.write("error: %s\n" % message)
-    sys.exit(1)
-
-
-def check_path(path: Path):
-    try:
-        path.exists()
-    except OSError as e:
-        error(str(e))
-
-
-def check_exists(path: Path, name: str):
-    check_path(path)
-    if not path.exists():
-        error("Path for {} does not exist: {}".format(name, str(path)))
-
-
-def check_file_if_exists(path: Path, name: str):
-    check_path(path)
-    if path.exists() and path.is_dir():
-        error("Path for {} exists but it is a directory: {}".format(name, str(path)))
-
-
-def check_dir_if_exists(path: Path, name: str):
-    check_path(path)
-    if path.exists() and not path.is_dir():
-        error("Path for {} exists but it is not a directory: {}".format(name, str(path)))
+import sys, os
 
 
 def load_hashes(path: Path):
@@ -109,3 +78,4 @@ if __name__ == "__main__":
     else:
         if is_compilation_required(args.input, args.output, args.hashes):
             print(1)
+
